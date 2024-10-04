9295c2ef9621a0527de20322a1dd889fc5dffa5d
SunnyBiolie <nguyenthanhdanh1789@gmail.com>
2024-10-04 18:08:42 +0700

    Comp: improved search form, change profile infor. Start-id: 126726a0-32cf-414a-9f4d-6a1a2a2b8d5b - end day 04/10

---
+++ app/api/books/get-by-id/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { failedJWTCheck, jwtCheck } from "@/lib/helper";
+import { missingFields } from "@/configs";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { isAuth } = await jwtCheck(req);
+    if (!isAuth) {
+      return failedJWTCheck();
+    }
+    const searchParams = req.nextUrl.searchParams;
+
+    const bookId = searchParams.get("bookId");
+
+    if (!bookId) return missingFields();
+
+    const book = await prisma.book.findUnique({
+      where: {
+        id: bookId,
+      },
+    });
+
+    return NextResponse.json({ data: book });
+  } catch (err) {
+    return NextResponse.json({ message: (err as Error).name }, { status: 500 });
+  }
+}
