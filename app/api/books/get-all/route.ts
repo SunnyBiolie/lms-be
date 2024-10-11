4906871f59017de9f6829707c87a9985312902ac
SunnyBiolie <nguyenthanhdanh1789@gmail.com>
2024-10-11 18:12:57 +0700

    Update-id: 126726a0-32cf-414a-9f4d-6a1a2a2b8d5b - end day 11/10

---
+++ app/api/books/get-all/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { failedJWTCheck, jwtCheck } from "@/lib/helper";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { isAuth } = await jwtCheck(req);
+    if (!isAuth) {
+      return failedJWTCheck();
+    }
+
+    const searchParams = req.nextUrl.searchParams;
+    const current = searchParams.get("current")
+      ? Number(searchParams.get("current"))
+      : 1;
+    const pageSize = searchParams.get("pageSize")
+      ? Number(searchParams.get("pageSize"))
+      : 5;
+
+    const total = await prisma.book.count();
+
+    const listBooks = await prisma.book.findMany({
+      skip: pageSize * (current - 1),
+      take: pageSize,
+      include: {
+        Categories: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json({
+      data: {
+        listBooks,
+        total,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ message: (err as Error).name }, { status: 500 });
+  }
+}
--- app/api/books/get-liked-by-acc-id/route.ts
+++ app/api/books/get-liked-by-acc-id/route.ts
@@ -1,17 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { failedJWTCheck, jwtCheck } from "@/lib/helper";
+import { missingFields } from "@/configs";
 
 export async function GET(req: NextRequest) {
   try {
-    const { isAuth } = await jwtCheck(req);
-    if (!isAuth) {
+    const { isAuth, account } = await jwtCheck(req);
+    if (!isAuth || !account) {
       return failedJWTCheck();
     }
 
     const searchParams = req.nextUrl.searchParams;
 
-    return NextResponse.json("");
+    const type = searchParams.get("type");
+    const accountId = searchParams.get("accountId");
+
+    if (!type || !accountId) return missingFields();
+
+    if (type === "owner" && account.id !== accountId) {
+      return NextResponse.json(
+        { message: "This collection is not yours" },
+        { status: 409 }
+      );
+    }
+
+    const listBooks = await prisma.book.findMany({
+      where: {
+        LikedBy: {
+          some: {
+            id: {
+              equals: accountId,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({ data: listBooks });
   } catch (err) {
     return NextResponse.json({ message: (err as Error).name }, { status: 500 });
   }
